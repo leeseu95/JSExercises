4125d5809b347af5a7c04df8b34e976d85419d02
Seung Lee <leeseu95@gmail.com>
2021-08-18 20:43:53 -0500

    Binary Tree delete function completed, tested with several tree forms

--- BinaryTrees/BinaryTreeCode.js
+++ BinaryTrees/BinaryTreeCode.js
@@ -6,6 +6,7 @@
  *          9               25
  *      5       12      23                   
  *          11      14
+ *               13     15
  ****************************************/
 
 function Node(value) { //Constructor for a node 
@@ -122,7 +123,7 @@ BinarySearchTree.prototype.delete = function(nodeVal) {
     //First Case scenario (if the node we want to delete is the leaf), then delete the node
     if(!currentNode.left && !currentNode.right) {
         var parentNode = currentNode.parent;
-        if(parentNode.left == currentNode) {
+        if(parentNode.left == currentNode) { //If the left leaf of the parent is our current node
             parentNode.left = null;
         } else {
             parentNode.right = null;
@@ -138,7 +139,7 @@ BinarySearchTree.prototype.delete = function(nodeVal) {
             } else if (parentNode.left == currentNode) { //If the current nodei s left leaf of the parent
                 parentNode.left = currentNode.left;
             }
-        } else if (currentNode.right) {
+        } else if (currentNode.right) { //If the irght leaf is the one we need to connect to the parent;
             if(parentNode.right == currentNode) { //If the current node is right leaf of the parent
                 parentNode.right = currentNode.right; 
             } else if (parentNode.left == currentNode) { //If the current nodei s left leaf of the parent
@@ -146,6 +147,53 @@ BinarySearchTree.prototype.delete = function(nodeVal) {
             }
         }
     }
+
+    //Third Case scenario (if the node has two leaves), we need to get the lowest value on the right subtree
+    if(currentNode.left && currentNode.right) {
+        var tempNode = currentNode.right;
+        while(tempNode.left) {
+            tempNode = tempNode.left;
+        }
+
+        //Once we have the lowest value on the right subtree, we have to connect the left and right of currentNode to this temp Node we have
+        if(tempNode.parent.left == tempNode) { //We delete the node we're on from it's parent
+            tempNode.parent.left = null;
+        } else {
+            tempNode.parent.right = null;
+        }
+
+        //Link the left and right leaves to the leaves that the node we want to delete has
+        if(currentNode.left != tempNode) { //Have to make sure that it doesnt link to itself
+            tempNode.left = currentNode.left;
+        }
+        if(currentNode.right != tempNode) {
+            tempNode.right = currentNode.right;
+        }
+
+        //Set the new left and right leaves to have the correct parents;
+        if(tempNode.left) {
+            tempNode.left.parent = tempNode;
+        }
+        if(tempNode.right) {
+            tempNode.right.parent = tempNode;
+        }
+
+        //If the node is root, we have to set it as the root and re-connect it with the parent
+        if(currentNode == this.root) { //If the node was the root, we have to place this as the root
+            this.root = tempNode;
+            tempNode.parent = null;
+        } else {
+            tempNode.parent = currentNode.parent;
+            if(currentNode.parent.left == currentNode) { //Set the parent of the node we're deleting to our new node
+                currentNode.parent.left = tempNode;
+            } 
+            if(currentNode.parent.right == currentNode) {
+                currentNode.parent.right = tempNode;
+            }
+        }
+        // console.log(tempNode);
+    }
+
     return true;
 }
 
@@ -159,6 +207,8 @@ bst.insert(12);
 bst.insert(11);
 bst.insert(14);
 bst.insert(23);
+bst.insert(13);
+bst.insert(15);
 
 //Test Delete of a node in tree that has no leaves
 // console.log(bst.delete(14));
@@ -166,9 +216,27 @@ bst.insert(23);
 // console.log(bst.getNodeByVal(12));
 
 //Test delete of a node in tree that has 1 leaf
-console.log(bst.delete(25));
-console.log(bst.getNodeByVal(25));
-console.log(bst.getNodeByVal(20));
+// console.log(bst.delete(25));
+// console.log(bst.getNodeByVal(25));
+// console.log(bst.getNodeByVal(20));
+
+//Test delete of a node in tree that has 2 leaves
+// console.log(bst.delete(20));
+// console.log(bst.getNodeByVal(20));
+// console.log(bst.getNodeByVal(23));
+// console.log(bst.root);
+
+//Test 2 of deleting a node in tree that has 2 leaves
+// console.log(bst.delete(12));
+// console.log(bst.getNodeByVal(12));
+// console.log(bst.getNodeByVal(9));
+// console.log(bst.root);
+
+//Test 3 of deleting a node in tree that has 2 leaves (deeper)
+console.log(bst.delete(12));
+console.log(bst.getNodeByVal(12));
+console.log(bst.getNodeByVal(13));
+console.log(bst.root);
 
 //Testing of finding IOS 
 // var test = bst.getNodeByVal(9);
